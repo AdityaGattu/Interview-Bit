006e17ad158f956d8db09b4c797b1de7beae2191
AdityaGattu <52188412+AdityaGattu@users.noreply.github.com>
2020-09-04 18:03:58 +0530

    findsubstring.cpp

---
+++ Strings/Implement StrStr.cpp
@@ -0,0 +1,36 @@
+int Solution::strStr(const string A, const string B) {
+    
+    if(A.find(B)!=string::npos)
+    {
+        int ans=A.find(B);
+        return ans;
+    }
+    return -1;
+}
+************************************************************
+
+sol2:
+
+int Solution::strStr(const string A, const string B) {
+    
+    int start=0;
+    int j=0;
+    int temp_start=start;
+    
+    while(temp_start<A.length() && j<B.length())
+    {
+        if(A[temp_start]==B[j])
+        {
+            temp_start++;
+            j++;
+        }
+        else
+        {
+            temp_start=++start;
+            j=0;
+        }
+        if(j==B.length()){return start;}
+    }
+    return -1;
+}
+
